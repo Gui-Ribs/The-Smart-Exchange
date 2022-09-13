fe29cc268f1d3609356778e135fde7afdad74394
Phoenix-141 <gui.ribs2000@gmail.com>
2022-09-12 23:24:38 -0300

    About-Image

--- AboutUs/index.php
+++ AboutUs/index.php
@@ -12,24 +12,35 @@
   ?>
 
 
-  <div class="container-fluid mb-4">
-    <div class="row" style ="margin: 2em;">
+  <div class="container-fluid">
+    <div class="row" style ="margin: 5em;">
       <div class="col-sm-4">
         <h1 class="mb-4">Sobre Nós</h1>
-      <p>
-        O The Smart Exchange é uma dos maiores projetos empresariais já visto pela história da humanidade.
-        Levamos em conta a capacidade humana, de tal forma concentramos todos os nossos esforços para fazerem com que as pessoas
-        liberem a total capacidade de seu cérebro. Afinal todos nós sabemos que para se ganhar nessa vida e produzir cada vez mais
-        agindo para a prosperidade da nação não é com sentimentalismos patéticos que se consegue tal virtude.
-        O intuito principal é levar ordem para sociedade atribuindo o racionalismo e o conhecimento cientifico para assim evoluirmos
-        tecnologicamente.
-        Nós somos a principal companhia de chips neuro-cerebrais de 2030, conseguimos este posto avançado pois acreditamos
-        que se passarmos os princípio fundamentais a cerca da racionalidade levaremos progressos aos lares.
-    </p>
+        <p>
+          O The Smart Exchange é uma dos maiores projetos empresariais já visto pela história da humanidade.
+          Levamos em conta a capacidade humana, de tal forma concentramos todos os nossos esforços para fazerem com que as pessoas
+          liberem a total capacidade de seu cérebro. Afinal todos nós sabemos que para se ganhar nessa vida e produzir cada vez mais
+          agindo para a prosperidade da nação não é com sentimentalismos patéticos que se consegue tal virtude.
+          O intuito principal é levar ordem para sociedade atribuindo o racionalismo e o conhecimento cientifico para assim evoluirmos
+          tecnologicamente.
+          Nós somos a principal companhia de chips neuro-cerebrais de 2030, conseguimos este posto avançado pois acreditamos
+          que se passarmos os princípio fundamentais a cerca da racionalidade levaremos progressos aos lares.
+        </p>
+      </div>
+      
+      <div class="col-sm-4">
+
+      </div>
+
+      <div class="col-sm-4">
+        <div class="limitea" style="max-width: 1000px;">
+          <img src="../Assets/img/neuralink-implante-cerebral.jpg" alt="" style=" max-width: 100%;">
+        </div>
       </div>
     </div>
   </div>
 
+  
   <?php include '../Components/footer.php'; ?>
 </body>
 
