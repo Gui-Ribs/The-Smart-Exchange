3ed92c107f63f9b8de8fc9b40bbd22dbdc0d82b9
lu-css <dossantossilvalucas963@gmail.com>
2022-09-16 16:37:42 -0300

    Xadrez challange

---
+++ Rising/justapawn/CHECKmate.php
@@ -0,0 +1,14 @@
+<?php
+  $res = $_POST['rook'];
+
+if($res == "francesa"){
+  // next challange
+  echo "Acerto";
+}
+else{
+  // loser LOL
+  header("location:\\the-smart-exchange/");
+
+}
+
+?>
---
+++ Rising/justapawn/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="king">
+    <img src="\the-smart-exchange/Assets/img/pawns.svg" />
+    <h1>The voice of Pawns</h1>
+    <h3>They know how communicate</h3>
+    <form class="bishop" method="POST" action="CHECKmate.php">
+      <input type="text" name="rook" class="rook" autocomplete="off">
+      <button class="queen">Checkmate</button>
+    </form>
+  </div>
+
+</body>
+
+</html>
