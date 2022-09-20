e1702f6eaf3edfda28287873e60625a37eb93a95
lu-css <dossantossilvalucas963@gmail.com>
2022-09-20 00:53:27 -0300

    Requiem funcional

---
+++ Rising/thegodexperience/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <title>Ci!lo de Dan"e </title>
+</head>
+<body>
+
+    <header>
+        <div id="instrucoes" class="instrucoes">
+            <h2><u>.nstruções</u>:</h2>
+            <p>As instruções devem ser seguidas ao pé da letra.
+                O ciclo de Dante visa selecionar os mais aptos para a
+                nossa sociedade nos tempos atuais. Aqueles que não passarem serão
+                considerados inaptos, e portanto, 1escartados.
+                Siga essas regras:
+                ...>!@l#rta: D@dos CorrompidoZ!>
+            </p>
+        </div>
+    </header>
+
+    <main>
+        <div id="corpo" class="corpo">
+            <form id="decifre" class="decifre">
+                <div class="form_text">
+                    <p>1#2Z</p>
+                    <p>#*</p>
+                    <p>K@!J.L@</p>
+                </div>
+                <div class="form_bootom">
+                    <input type="text" value=""></input>
+                    <input type="submit" id="btndecifrar" value="@B@.*O">
+                </div>
+            </form>
+
+            <!-- Respostas aceitáveis: 
+                Deus Ex Machina; 
+                deus Ex Machina; 
+                deus ex Machina; 
+                Deus Ex machina; 
+                Deus ex machina;
+                deus ex machina; 
+                deus Ex machina.
+            -->
+        </div>
+
+        <div class="main_avancar">
+            <input type="submit" id="prosseguir" value="Avancar">
+        </div>
+
+        <footer>
+            <p>Os resultados serão liberados 30 minutos após a conclusão da eliminação</p>
+        </footer>
+    </main>
+</body>
+</html>
